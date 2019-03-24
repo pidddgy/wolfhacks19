7c36807f6af492cf5e5007e539500c431e4161f8
Marcus Chong <marcus.jn.chong@gmail.com>
2019-03-24 11:13:10 -0400

    firebaseeeee

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("hi")
+const config = {
+    apiKey: "AIzaSyD--fcGZUlW6qZny-GRCaHZhOq498ZBENw",
+    authDomain: "wolfhacks19.firebaseapp.com",
+    databaseURL: "https://wolfhacks19.firebaseio.com",
+    projectId: "wolfhacks19",
+    storageBucket: "wolfhacks19.appspot.com",
+    messagingSenderId: "189830752995"
+  };
+
+firebase.initializeApp(config);
+
