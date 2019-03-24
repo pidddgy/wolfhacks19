6b29c3bb9cb4d0a776ffb4cbcc4869bafea84085
Johnny Qu <johnnyqu4@gmail.com>
2019-03-24 16:12:29 -0400

    added the token and extracted data

--- js/translate.js
+++ js/translate.js
@@ -1,9 +1,9 @@
-function translate(frm, to, msg, token){
+function translate(frm, to, msg){
 	var http = new XMLHttpRequest();
 	const url = "https://translation.googleapis.com/language/translate/v2";
 	http.open ("POST", url, false);
 	http.setRequestHeader("Content-Type", "application/json");
-	http.setRequestHeader("Authorization", `Bearer ${token}`);
+	http.setRequestHeader("Authorization", `Bearer ya29.c.ElrWBm7kCWbbTDN8nfuF-rRiQVtXG5BDWwQVxqn7VaDlHJ1c6MSiCT_1PYxnlBbNxlZo9a2WctL946pZha7j7JxIrsUmDwCqvvK6v8iUUfTg1SnV6UexlJRvjRA`);
 	http.send(JSON.stringify({"q": `${msg}`, "source": `${frm}`, "target": `${to}`, "format": "text"}));
-	return JSON.parse(http.responseText);
+	return JSON.parse(http.responseText).data.translations[0].translatedText;
 }
