eed7dfa67cc8478ab37772b2a7fb73b20ac20885
Johnny Qu <johnnyqu4@gmail.com>
2019-03-24 15:47:58 -0400

    api works

--- js/translate.js
+++ js/translate.js
@@ -1,10 +1,9 @@
-function translate(frm, to, msg){
+function translate(frm, to, msg, token){
 	var http = new XMLHttpRequest();
 	const url = "https://translation.googleapis.com/language/translate/v2";
-	http.open ("POST", url, true);
+	http.open ("POST", url, false);
 	http.setRequestHeader("Content-Type", "application/json");
-	http.setRequestHeader("Authorization", "Bearer ya29.c.ElrWBtj03j-Ml9u4AmPOFCyoLhnG7WqcZToCOi6f1TnS1D5O8Zms1A_dvyDJSAXE-E4FcObOwf0Pk4Ly38_b9H5nZRpqQL28DemBCjVaX2oLCq8xQorTC5K8Kx0");
-	http.send({'q': `${msg}`,'source': `${frm}`,'target': `${to}`,'format': 'text'});
-	return xmlHttp.responseText;
+	http.setRequestHeader("Authorization", `Bearer ${token}`);
+	http.send(JSON.stringify({"q": `${msg}`, "source": `${frm}`, "target": `${to}`, "format": "text"}));
+	return JSON.parse(http.responseText);
 }
-console.log(translate("en", "zh", "hello"))
