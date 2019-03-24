cb1c97ebbbe506469e291ebb0f3b84e37191805a
Bert Sun <bertsunjc@gmail.com>
2019-03-24 18:58:33 -0400

    added translation when typing
    added translation api to translate textbox before submit

--- js/main.js
+++ js/main.js
@@ -7,6 +7,50 @@ const config = {
     messagingSenderId: "189830752995"
   };
 
+var token = "Bearer ya29.c.ElrXBj1BZGO0J-JiPJ-TWO0vmTGCvz-KIP3COJlFQ_GCk9mdtHM8Rj0lARucQet3LXMy-ZHGHw8XCt1eUq_JijRC-qaAP269Ul2TCgWugdJcd5QkLvFlt6JCm8c";
+
+        function translateToEng(msg){
+    var from = detectLang(msg);
+    var http = new XMLHttpRequest();
+    const url = "https://translation.googleapis.com/language/translate/v2";
+    http.open ("POST", url, false);
+    http.setRequestHeader("Content-Type", "application/json");
+    http.setRequestHeader("Authorization", token);
+    http.send(JSON.stringify({"q": `${msg}`, "source": from, "target": "en", "format": "text"}));
+    console.log(http.responseText);
+    return JSON.parse(http.responseText).data.translations[0].translatedText;
+}
+
+
+
+
+function detectLang(msg){
+    var http = new XMLHttpRequest();
+    const url = "https://translation.googleapis.com/language/translate/v2/detect";
+    http.open ("POST", url, false);
+
+    http.setRequestHeader("Authorization", token);
+    http.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+    http.send(JSON.stringify({"q": `${msg}`}));
+    console.log(http.responseText);
+    return JSON.parse(http.responseText).data.detections[0].language;
+}
+    function updateToken(tokenStr){
+        token = "Bearer " + tokenStr;
+    }
+
+    function translateFromEng(engStr, lang){
+    var http = new XMLHttpRequest();
+    const url = "https://translation.googleapis.com/language/translate/v2";
+    http.open ("POST", url, false);
+    http.setRequestHeader("Content-Type", "application/json");
+    http.setRequestHeader("Authorization", token);
+    http.send(JSON.stringify({"q": `${engStr}`, "source": "en", "target": `${lang}`, "format": "text"}));
+    console.log(http.responseText);
+    return JSON.parse(http.responseText).data.translations[0].translatedText;
+    }
+
+
 firebase.initializeApp(config);
 const db = firebase.database();
 
@@ -69,9 +113,11 @@ demoRef.on('value', function(ss) {
   
 
 function sendMsg() {
+	var messageTxt = document.getElementById("messageSender").value;
+	messageTxt = translateToEng(messageTxt);
   db.ref("demo").push({
     author: firebase.auth().currentUser.uid,
-    text: document.getElementById("messageSender").value,
+    text: messageTxt,
   }) 
 
   document.getElementById("messageSender").value = "";
