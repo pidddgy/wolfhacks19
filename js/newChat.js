5f53179adcce1c9d79aab57447d3da5bff688bd9
Marcus Chong <marcus.jn.chong@gmail.com>
2019-03-24 12:31:53 -0400

    sample push

--- js/newChat.js
+++ js/newChat.js
@@ -1,3 +1,8 @@
-function newChat() {
-    console.log("HI");
+const newChat = () => {
+    const db = firebase.database();
+    
+    db.ref('/groups/').push({
+            messages: ["hi", "hello", "asdf"],
+            people: [12, 123, 14]
+    })
 }
